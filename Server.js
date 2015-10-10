bfa40f4e5426ee8cb9d77c690d2dc5e6baf0e4a0
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-10-10 00:15:08 -0600

    Documentación
    Se comenta el código

--- Server.js
+++ Server.js
@@ -1,68 +1,98 @@
-     var http = require('http');
-     var port=process.env.PORT;
-     var querystring = require('querystring');
-     var utils = require('util');
-     var url = require("url");
+var http = require('http');
+var port=process.env.PORT;
+var querystring = require('querystring');
+var utils = require('util');
+var url = require("url");
       
-      var server = http.createServer(function(req, res) {
-       switch (req.url) { //checking the request url
-         case '/':
-          homePageHandler (req, res); //handler for home page
-          break;
-        case '/register':  
-          registerFormHandler (req, res);//hamdler for register
-          break;
-        default:
-          nofoundHandler (req, res);// handler for 404 not found
-          break;
-      }
+var server = http.createServer(function(req, res) 
+{
+  switch (req.url) 
+  { 
+    // se verifica la solicitud
+
+    // home page
+    case '/':
+      homePageHandler (req, res); 
+      break;
+    
+    // registro
+    case '/register':  
+      registerFormHandler (req, res);
+      break;
+    
+    // not found si accede a algo que no existe
+    default:
+      nofoundHandler (req, res);
+      break;
+  }
+
+});
+
+server.listen(port);
+
+// función que despliega el formulario
+function homePageHandler (req, res) 
+{
+  
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  // fomulario a desplegar
+  var body = '<html>'+
+  '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+  '</head>'+
+  '<body>'+
+    '<form action="/register" method="post">'+
+      'Nombre:<input type=text value="" name="name" size=15></br>'+
+      'Email:<input type=text value="" name="email" size=15></br>'+
+      '<input type="submit" value="Agregar" />'+
+    '</form>'+
+  '</body>'+
+  '</html>';
+  
+  // contenido de la respuesta
+  res.end(body);
+}
+    
+// funcion que maneja el post
+function registerFormHandler (req, res) 
+{
+    // en caso que se desee conocer de que controlador vino la solicitud
+    //var pathname = url.parse(req.url).pathname;
+
+    var postData = "";   
+
+    req.on('data', function(chunk) 
+    {
+
+      // agregamos los daton que recibimos
+      postData += chunk.toString();
+
+    });   
+    
+    req.on('end', function() 
+    {
+
+      // ... proceso de datos
+      res.writeHead(200, "OK", {'Content-Type': 'text/html'});      
+      
+      // parse de los datos enviados
+      var decodedBody = querystring.parse(postData);      
+      
+      // output de los datos decodificados         
+      res.write('<html><head><title>Datos enviados</title></head><body><pre>');
+      res.write(utils.inspect(decodedBody));    
+      res.write('</pre></body></html>');       
+      res.end();
+
     });
-    server.listen(port);
-    //function to display the html form
-    function homePageHandler (req, res) {
-    console.log('Request handler home was called.');
-      res.writeHead(200, {'Content-Type': 'text/html'});
-       var body = '<html>'+
-        '<head>'+
-        '<meta http-equiv="Content-Type" content="text/html; '+
-        'charset=UTF-8" />'+
-        '</head>'+
-        '<body>'+
-        '<form action="/register" method="post">'+
-        'Name:<input type=text value="" name="name" size=15></br>'+
-        'Email:<input type=text value="" name="email" size=15></br>'+
-        '<input type="submit" value="Submit" />'+
-        '</form>'+
-        '</body>'+
-        '</html>';
-    //response content
-    res.end(body);
-    }
-    //handler for Post request
-    function registerFormHandler (req, res) {
-    console.log('Request handler register was called.');    
-    var pathname = url.parse(req.url).pathname;
-    console.log("Request for " + pathname + " received.");    
-     var postData = "";    
-      req.on('data', function(chunk) {
-          // append the current chunk of data to the postData variable
-          postData += chunk.toString();
-        });   
-        req.on('end', function() {   
-       // doing something with the posted data
-          res.writeHead(200, "OK", {'Content-Type': 'text/html'});      
-          // parse the posted data
-          var decodedBody = querystring.parse(postData);      
-          // output the decoded data to the HTTP response          
-          res.write('<html><head><title>Post data</title></head><body><pre>');
-          res.write(utils.inspect(decodedBody));    
-          res.write('</pre></body></html>');       
-          res.end();
-        });
-    }
-    //Error handler for 404 no found
-    function nofoundHandler(req, res) {
-      console.log('Request handler nofound was called.');    
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('404 Error - Request handler not found'); 
-    }
+}
+
+// handler para controlador que no existe
+function nofoundHandler(req, res) 
+{
+      
+  res.writeHead(404, {'Content-Type': 'text/plain'});
+  res.end('404 Error - Request handler not found'); 
+}
